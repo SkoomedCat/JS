bb63ddcc47dadbc9505422f57dded155a465fe2d
Alex <aoprohvatkin@yandex.ru>
2019-12-26 16:00:35 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,21 @@
+var leftBorderWidth = 1;
+
+const pi = 3.14;
+let second =2; 
+let person = {
+    name: 'John',
+    age: 25,
+    isMarred: false
+};
+
+console.log(person['name']);
+
+let arr = ['plum.png', 'orange.jpg','apple.bpm'];
+
+//console.log(arr[2]) 
+
+//alert("Hello world!");
+
+let answer = prompt("Есть ли вам 18?", 'Да');
+
+console.log(typeof(null));
